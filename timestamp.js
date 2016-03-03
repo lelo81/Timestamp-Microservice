a90ec4ec85a677d440b60858b7386c91e87c53b4
lelo81 <le_lo@hotmail.com>
2016-03-03 00:51:02 +0000

    modifications

--- timestamp.js
+++ timestamp.js
@@ -1,10 +1,34 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+var url = require('url');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var server = http.createServer(function (req, res) {
+    if (req.method != 'GET')
+        return res.end('send me a GET\n');
+        
+    var path = url.parse(req.url,true).pathname;
+    console.log(path);
+    
+    var jsonString = {
+        unix: null,
+        natural: null
+    };
+    
+
+    if (path.indexOf('%20') == -1) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        jsonString.unix = "bla";
+        jsonString.natural = "bla";
+        res.end(JSON.stringify(jsonString));
+    }
+    else {
+        console.log("Entrei no else");
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        jsonString.unix = "ble";
+        jsonString.natural = "ble";
+        res.end(JSON.stringify(jsonString));
+    }
+
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+server.listen(8080);
+console.log("App listening on port 8080!")
