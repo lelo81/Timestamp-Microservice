0a4485f5a2e4d6e05742935f3353269614fb9559
lelo81 <le_lo@hotmail.com>
2016-03-19 23:25:15 +0000

    Last changes to complete code

--- timestamp.js
+++ timestamp.js
@@ -1,34 +1,46 @@
 var http = require('http');
 var url = require('url');
+require('datejs');
 
 var server = http.createServer(function (req, res) {
     if (req.method != 'GET')
         return res.end('send me a GET\n');
         
     var path = url.parse(req.url,true).pathname;
-    console.log(path);
-    
+    path = path.substr(1,path.length);
+
     var jsonString = {
         unix: null,
         natural: null
     };
     
 
-    if (path.indexOf('%20') == -1) {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        jsonString.unix = "bla";
-        jsonString.natural = "bla";
-        res.end(JSON.stringify(jsonString));
+    if (path.indexOf('%20') != -1) {
+        var strDate = path.replace(/%20/g,' ');
+        var date = Date.parse(strDate);
+        console.log(date);
+        
+        if (date != null) {
+            jsonString.unix = date.getTime()/1000;
+            jsonString.natural = date.toString("MMMM") + " " + date.getDate() + ", " + date.getFullYear();
+        }
+
     }
     else {
-        console.log("Entrei no else");
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        jsonString.unix = "ble";
-        jsonString.natural = "ble";
-        res.end(JSON.stringify(jsonString));
+        var date = new Date();
+        date.setTime(+path*1000);
+
+        if (date != 'Invalid Date') {
+            jsonString.unix = +path;
+            jsonString.natural = date.toString("MMMM") + " " + date.getDate() + ", " + date.getFullYear();
+        }
     }
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(jsonString));
+
 
 });
 
-server.listen(8080);
-console.log("App listening on port 8080!")
+server.listen(process.env.PORT);
+console.log("App listening on IP:" + process.env.IP + " and PORT:" + process.env.PORT);
